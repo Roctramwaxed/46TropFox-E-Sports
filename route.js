4e65c527acda2c463d601be5b9767d7a6b362cdc
vikryprakasa <vikryprakasa31@gmail.com>
2020-08-28 05:36:32 +0700

    Edit team

--- route.js
+++ route.js
@@ -1,5 +1,23 @@
 const route = require('express').Router()
 
-route.get('/')
+route.get('/',(req,res) =>{
+    res.render('homePage')
+})
+
+route.get('/teamPage',(req,res)=>{
+    res.render('./teamViews/teamPage')
+})
+
+route.get('/games',(req,res)=>{
+    res.render('./gameViews/gamePage')
+})
+
+route.get('/teams/login',(req,res)=>{
+    res.render('login')
+})
+
+route.get('/teams/register',(req,res)=>{
+    res.render('register')
+})
 
 module.exports = route
