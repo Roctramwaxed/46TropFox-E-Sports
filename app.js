59737b0fc73c4e70f02056d911b12bd6d80d29b8
vikryprakasa <vikryprakasa31@gmail.com>
2020-08-28 02:39:54 +0700

    committ

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const route = require('./route')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.get('/',(req,res) =>{
+    res.render('homePage')
+})
+
+app.get('/teamPage',(req,res)=>{
+    res.render('./teamViews/teamPage')
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
